c4fcc3a8cd732673f411c9e0451967edb6d74646
Ashlin Aronin & Julio Diaz <student@epicodus.com>
2015-09-23 11:34:47 -0700

    clean up code

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -1,9 +1,11 @@
-courseRoster.controller('StudentsCtrl', function StudentsCtrl($scope, $stateParams, CoursesFactory, UtilitiesFactory) {
-	$scope.course = UtilitiesFactory.findById(CoursesFactory.courses, $stateParams.courseId);
-	$scope.addStudent = function() {
-		$scope.course.students.push({ name: $scope.studentName });
-		
-		// Reset input box
-		$scope.studentName = null;
+courseRoster.controller('StudentsCtrl',
+	function StudentsCtrl($scope, $stateParams, CoursesFactory, UtilitiesFactory) {
+		$scope.course = UtilitiesFactory.findById(CoursesFactory.courses, $stateParams.courseId);
+		$scope.addStudent = function() {
+			$scope.course.students.push({ name: $scope.studentName });
+			
+			// Reset input box
+			$scope.studentName = null;
+		}
 	}
-});
+);
